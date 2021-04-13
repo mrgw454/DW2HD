116161a649d6360609699ff1777407e83ee0c09a
Ron Klein <ron@kdomain.org>
2021-04-13 15:29:14 -0500

    removing outdated files

--- recursive-unzip.sh
+++
@@ -1,3 +0,0 @@
-find . -name "*.zip" | while read filename; do unzip -o -d "`dirname "$filename"`" "$filename"; done;
-#find . -name "*.zip" | xargs -P 5 -I fileName sh -c 'unzip -o -d "$(dirname "fileName")/$(basename -s .zip "fileName")" "fileName"'
-
